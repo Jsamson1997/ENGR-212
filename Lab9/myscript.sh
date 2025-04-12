89aee1fbea7d0b8d8737d1db8756e6284f2f5574
Joshua Samson <923053622@sfsu.edu>
2025-04-12 16:26:18 -0700

    Lab9 commit

---
+++ Lab9/myscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get absolute path and prints
+realpath "$1"
+
+# Print contents
+ls "$1"
---
+++ Lab9/pwd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Prints current directory
+pwd
+
+# Reads current directory content 
+ls -l
